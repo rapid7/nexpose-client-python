b0fe299387570f2a99ae5d9d882e87f7328c6a12
Gavin Schneider <gavin_schneider@rapid7.com>
2017-11-17 13:13:03 -0800

    Ignore flake8 for the unused ex vars

--- nexpose/nexpose.py
+++ nexpose/nexpose.py
@@ -284,7 +284,7 @@ class NexposeSessionBase(object):
         try:
             response = OpenWebRequest(self._URI_root, None, {}, self.timeout)
             return NexposeStatus.GetStatusFromURL(response.geturl())
-        except Exception as ex:
+        except Exception as ex:  # noqa: F841
             return NexposeStatus.UNKNOWN
 
 
@@ -1466,7 +1466,7 @@ class NexposeSession(NexposeSession_APIv1d2):
             asset_group.description = json_dict.get['description']
             if asset_group.description is None:
                 asset_group.description = asset_group.short_description
-        except Exception as ex:
+        except Exception as ex:  # noqa: F841
             pass
 
         return asset_group
